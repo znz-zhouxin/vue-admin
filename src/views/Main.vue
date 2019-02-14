102e7b9f7e305ed26f4ed646a918494b4bca9118
zhouxin <xin.zhou@striking.ly>
2019-02-14 20:20:41 +0800

    feat: add main view

---
+++ src/views/Main.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-container>
+    <el-header>
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="1"><router-link to="/home">Home</router-link></el-menu-item>
+        <el-menu-item index="2"><router-link to="/about">About</router-link></el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-container>
+      <router-view/>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+      }
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.el-header {
+  padding: 0 0 0;
+}
+</style>
